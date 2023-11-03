c5e0432e860e096fc7e2c3fc1a8f4d66feb47d31
MATSUOKA Takashi <matsujirushi@live.jp>
2023-11-04 08:31:14 +0900

    Jukebox修正

--- platformio/Jukebox/src/main.cpp
+++ platformio/Jukebox/src/main.cpp
@@ -15,47 +15,49 @@
 
 enum class Pitch
 {
-    OFF = 0,
+    REST = 0,
     C4 = 262,
     C4s = 277,
-    D4f = 277,
+    D4b = 277,
     D4 = 294,
     D4s = 311,
-    E4f = 311,
+    E4b = 311,
     E4 = 330,
     F4 = 349,
     F4s = 370,
-    G4f = 370,
+    G4b = 370,
     G4 = 392,
     G4s = 415,
-    A4f = 415,
+    A4b = 415,
     A4 = 440,
     A4s = 466,
-    B4f = 466,
+    B4b = 466,
     B4 = 494,
     C5 = 523,
     C5s = 554,
-    D5f = 554,
+    D5b = 554,
     D5 = 587,
     D5s = 622,
-    E5f = 622,
+    E5b = 622,
     E5 = 659,
     F5 = 698,
     F5s = 740,
-    G5f = 740,
+    G5b = 740,
     G5 = 784,
     G5s = 831,
-    A5f = 831,
+    A5b = 831,
     A5 = 880,
     A5s = 932,
-    B5f = 932,
+    B5b = 932,
     B5 = 988,
 };
+
+typedef int Duration;
 
 struct Note
 {
     Pitch pitch;
-    int duration;
+    Duration duration;
 };
 
 static const Note MelodyItsyBitsySpider[] =
@@ -65,15 +67,15 @@ static const Note MelodyItsyBitsySpider[] =
     {Pitch::G4, 3}, {Pitch::F4, 1}, {Pitch::G4, 3}, {Pitch::A4, 1},
     {Pitch::F4, 3}, {Pitch::F4, 1}, {Pitch::C4, 4},
 
-    {Pitch::A4, 3}, {Pitch::A4, 1}, {Pitch::A4, 3}, {Pitch::B4f, 1},
+    {Pitch::A4, 3}, {Pitch::A4, 1}, {Pitch::A4, 3}, {Pitch::B4b, 1},
     {Pitch::C5, 3}, {Pitch::C5, 1}, {Pitch::C5, 3}, {Pitch::C5, 1},
-    {Pitch::B4f, 3}, {Pitch::A4, 1}, {Pitch::B4f, 3}, {Pitch::C5, 1},
-    {Pitch::A4, 4}, {Pitch::OFF, 2}, {Pitch::C5, 2},
+    {Pitch::B4b, 3}, {Pitch::A4, 1}, {Pitch::B4b, 3}, {Pitch::C5, 1},
+    {Pitch::A4, 4}, {Pitch::REST, 2}, {Pitch::C5, 2},
 
-    {Pitch::A4, 4}, {Pitch::OFF, 2}, {Pitch::C5, 2},
-    {Pitch::A4, 4}, {Pitch::OFF, 2}, {Pitch::C5, 2},
+    {Pitch::A4, 4}, {Pitch::REST, 2}, {Pitch::C5, 2},
+    {Pitch::A4, 4}, {Pitch::REST, 2}, {Pitch::C5, 2},
     {Pitch::A4, 2}, {Pitch::C5, 2}, {Pitch::A4, 2}, {Pitch::C5, 2},
-    {Pitch::A4, 4}, {Pitch::OFF, 4},
+    {Pitch::A4, 4}, {Pitch::REST, 4},
 };
 
 static constexpr int Tempo = 150;
